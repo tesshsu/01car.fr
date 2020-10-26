99b102ee7175f01eb3074eb3d519c5ac5a24248f
tesshsu <tesshsu@gmail.com>
2020-10-26 15:48:48 +0100

    #8 Set env front

---
+++ front/.env.pro
@@ -0,0 +1,1 @@
+01CAR_APP_API_URL=http://localhost:3000/backend/public
--- front/components/Cards/CardAnnonceSlider.js
+++ front/components/Cards/CardAnnonceSlider.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import { baseUrl } from "./config";
+//import { baseUrl } from "./config";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
