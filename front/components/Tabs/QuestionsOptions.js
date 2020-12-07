3f44a129e7a089a3234e5f8719999b3ffe88d398
tesshsu <tesshsu@gmail.com>
2020-12-07 08:27:59 +0100

    #54 logical changed to toggle for equipement options

--- front/components/Tabs/QuestionsOptions.js
+++ front/components/Tabs/QuestionsOptions.js
@@ -4,14 +4,8 @@ import { Form, Field } from 'react-final-form';
 import {exterieur_equipements, interieur_equipements, securite_equipements, antivol_equipements, confort_equipements, autre_equipements} from 'helpers/constant';
 import useVendre from 'service/hooks/useVendre';
 import * as constant from 'helpers/constant';
-import * as formValidate from 'helpers/formValidate';
-//import {Condition, Error} from 'helpers/formValidate';
+import {Condition} from 'helpers/formValidate';
 
-const Condition = ({ when, is, children }) => (
-    <Field name={when} subscription={{ value: true }}>
-      {({ input: { value } }) => (value === is ? children : null)}
-    </Field>
-)
 
 const QuestionsOptions = ({dispatch, loading, response, hasErrors}) => {
   //submit
@@ -36,32 +30,40 @@ const QuestionsOptions = ({dispatch, loading, response, hasErrors}) => {
 		  }
 		}
 	  }
+	
   return (
     <>
       <div className="justify-center flex flex-wrap">
 		  <div className="w-full">
                     <Form
+					  initialValues={{
+						exterieur_equipements: [],
+						interieur_equipements: [],
+						securite_equipements: [],
+						antivol_equipements: [],
+						confort_equipements: [],
+						autre_equipements: [],
+					}}
 					  onSubmit={onSubmit}
 					  render={({ handleSubmit, form, submitting, pristine, values }) => (
 					      <form onSubmit={handleSubmit}>
 								<div className="w-full px-4">
 									<label
 										className="uppercase text-gray-700 text-sm"
-										htmlFor="question-7-1"
+										htmlFor="interieur_equipements"
 									>
 										Q7 - 1 ÉQUIPEMENTS EXTÉRIEUR ET CHÂSSIS
 									</label>
-									<Field name="question-7-1" component="input" type="checkbox" className="ml-2 form-checkbox">
+									<Field name="interieur_equipements" component="checkbox" type="checkbox" value={values.exterieur_equipements} className="ml-2 form-checkbox">
                                     </Field>
 									<div className="relative flex w-full flex-wrap items-stretch mb-3">
-                                      <Condition when="question-7-1" is={true} className="mt-2">
+                                      <Condition when="exterieur_equipements" is={true} className="mt-2">
                                         <div className="relative flex w-full flex-wrap items-stretch mb-3 px-2 mt-2">
 											 {exterieur_equipements.map(exterieur_equipement => (
 											    <label className="border border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight">
 												<Field
-												  name="exterieur_equipement"
+												  name="interieur_equipements"
 												  component="input"
-												  type="checkbox"
 												  value={exterieur_equipement.value}
 												/>{' '}
 												  {exterieur_equipement.name}
@@ -75,19 +77,19 @@ const QuestionsOptions = ({dispatch, loading, response, hasErrors}) => {
 							  <div className="w-full px-4">
 								  <label
 									  className="uppercase text-gray-700 text-sm"
-									  htmlFor="question-7-2"
+									  htmlFor="interieur_equipements"
 								  >
 									  Q7 - 2 Intérieur
 								  </label>
-								  <Field name="question-7-2" component="input" type="checkbox" className="ml-2 form-checkbox">
+								  <Field name="interieur_equipements" component="input" type="checkbox" className="ml-2 form-checkbox">
 								  </Field>
 								  <div className="relative flex w-full flex-wrap items-stretch mb-3">
-									  <Condition when="question-7-2" is={true} className="mt-2">
+									  <Condition when="interieur_equipements" is={true} className="mt-2">
 										  <div className="relative flex w-full flex-wrap items-stretch mb-3 px-2 mt-2">
 											  {interieur_equipements.map(interieur_equipement => (
 												  <label className="border border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight">
 													  <Field
-														  name={interieur_equipement.name}
+														  name="interieur_equipements"
 														  component="input"
 														  type="checkbox"
 														  value={interieur_equipement.value}
@@ -103,19 +105,19 @@ const QuestionsOptions = ({dispatch, loading, response, hasErrors}) => {
 							  <div className="w-full px-4">
 								  <label
 									  className="uppercase text-gray-700 text-sm"
-									  htmlFor="question-7-3"
+									  htmlFor="securite_equipements"
 								  >
 									  Q7 - 3 Sécurité
 								  </label>
-								  <Field name="question-7-3" component="input" type="checkbox" className="ml-2 form-checkbox">
+								  <Field name="securite_equipements" component="input" type="checkbox" className="ml-2 form-checkbox">
 								  </Field>
 								  <div className="relative flex w-full flex-wrap items-stretch mb-3">
-									  <Condition when="question-7-3" is={true} className="mt-2">
+									  <Condition when="interieur_equipements" is={true} className="mt-2">
 										  <div className="relative flex w-full flex-wrap items-stretch mb-3 px-2 mt-2">
-											  {securite_equipements.map(securite_equipement => (
+											  {interieur_equipements.map(securite_equipement => (
 												  <label className="border border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight">
 													  <Field
-														  name={securite_equipement.name}
+														  name="securite_equipements"
 														  component="input"
 														  type="checkbox"
 														  value={securite_equipement.value}
@@ -131,19 +133,19 @@ const QuestionsOptions = ({dispatch, loading, response, hasErrors}) => {
 								  <div className="w-full px-4">
 									  <label
 										  className="uppercase text-gray-700 text-sm"
-										  htmlFor="question-7-4"
+										  htmlFor="antivol_equipements"
 									  >
 										  Q7 - 4 Antivol
 									  </label>
-									  <Field name="question-7-4" component="input" type="checkbox" className="ml-2 form-checkbox">
+									  <Field name="antivol_equipements" component="input" type="checkbox" className="ml-2 form-checkbox">
 									  </Field>
 									  <div className="relative flex w-full flex-wrap items-stretch mb-3">
-										  <Condition when="question-7-4" is={true} className="mt-2">
+										  <Condition when="interieur_equipements" is={true} className="mt-2">
 											  <div className="relative flex w-full flex-wrap items-stretch mb-3 px-2 mt-2">
-												  {antivol_equipements.map(antivol_equipement => (
+												  {interieur_equipements.map(antivol_equipement => (
 													  <label className="border border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight">
 														  <Field
-															  name={antivol_equipement.name}
+															  name="antivol_equipements"
 															  component="input"
 															  type="checkbox"
 															  value={antivol_equipement.value}
@@ -159,19 +161,19 @@ const QuestionsOptions = ({dispatch, loading, response, hasErrors}) => {
 							  <div className="w-full px-4">
 								  <label
 									  className="uppercase text-gray-700 text-sm"
-									  htmlFor="question-7-5"
+									  htmlFor="confort_equipements"
 								  >
 									  Q7- 5 CONFORT
 								  </label>
-								  <Field name="question-7-5" component="input" type="checkbox" className="ml-2 form-checkbox">
+								  <Field name="confort_equipements" component="input" type="checkbox" className="ml-2 form-checkbox">
 								  </Field>
 								  <div className="relative flex w-full flex-wrap items-stretch mb-3">
-									  <Condition when="question-7-5" is={true} className="mt-2">
+									  <Condition when="confort_equipements" is={true} className="mt-2">
 										  <div className="relative flex w-full flex-wrap items-stretch mb-3 px-2 mt-2">
 											  {confort_equipements.map(confort_equipement => (
 												  <label className="border border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight">
 													  <Field
-														  name={confort_equipement.name}
+														  name="confort_equipements"
 														  component="input"
 														  type="checkbox"
 														  value={confort_equipement.value}
@@ -187,11 +189,11 @@ const QuestionsOptions = ({dispatch, loading, response, hasErrors}) => {
 							  <div className="w-full px-4">
 								  <label
 									  className="uppercase text-gray-700 text-sm"
-									  htmlFor="question-7-6"
+									  htmlFor="autre_equipements"
 								  >
 									  Q7 -6 AUTRES
 								  </label>
-								  <Field name="question-7-6" component="input" type="checkbox" className="ml-2 form-checkbox">
+								  <Field name="autre_equipements" component="input" type="checkbox" className="ml-2 form-checkbox">
 								  </Field>
 								  <div className="relative flex w-full flex-wrap items-stretch mb-3">
 									  <Condition when="question-7-6" is={true} className="mt-2">
@@ -199,7 +201,7 @@ const QuestionsOptions = ({dispatch, loading, response, hasErrors}) => {
 											  {autre_equipements.map(autre_equipement => (
 												  <label className="border border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight">
 													  <Field
-														  name={autre_equipement.name}
+														  name="autre_equipements"
 														  component="input"
 														  type="checkbox"
 														  value={autre_equipement.value}
@@ -219,5 +221,12 @@ const QuestionsOptions = ({dispatch, loading, response, hasErrors}) => {
     </>
   );
 }
+
+/*const mapStateToProps = (state) => ({
+  loading: state.response.loading,
+  response: state.response.response,
+  hasErrors: state.response.hasErrors,
+})
+export default connect(mapStateToProps)(QuestionsOptions)*/
 
 export default QuestionsOptions
