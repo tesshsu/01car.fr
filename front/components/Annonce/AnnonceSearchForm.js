100e1b2f226e76e4274e6f8ec035df27bb61ca57
tesshsu <tesshsu@gmail.com>
2021-01-25 13:07:25 +0100

    #114 Filter complete

--- front/components/Annonce/AnnonceSearchForm.js
+++ front/components/Annonce/AnnonceSearchForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Form, Field } from 'react-final-form';
 import Link from "next/link";
+import {fuelOptions, kmFilterOptions, priceFilterOptions, boiteFilterOptions, statusFilterOptions, minYearFilterOptions, maxYearFilterOptions} from "../../helpers/constant";
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 const onSubmit = async values => {
 	await sleep(300)
@@ -8,6 +9,8 @@ const onSubmit = async values => {
 }
 
 export default function AnnonceSearchForm() {
+	const [navbarOpen, setNavbarOpen] = React.useState(false);
+
 	return (
 		<>
 			<section className="mt-16">
@@ -19,7 +22,7 @@ export default function AnnonceSearchForm() {
 						render={({ handleSubmit, form, submitting, pristine, values }) => (
 							<form onSubmit={handleSubmit}>
 								<div className="flex flex-wrap mt-4">
-									<div className="w-full px-4 flex-1">
+									<div className="w-full px-2 flex-1">
 										<Field name="search">
 											{({ input, meta }) => (
 												<div className="relative w-full mb-3">
@@ -27,13 +30,31 @@ export default function AnnonceSearchForm() {
 														{...input}
 														type="number"
 														className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
-														placeholder="Marque Model ..."
+														placeholder="Marque..."
 													/>
 												</div>
 											)}
 										</Field>
 									</div>
-									<div className="w-full px-4 flex-1">
+									<div className="w-full px-2 flex-1">
+										<Field name="search">
+											{({ input, meta }) => (
+												<div className="relative w-full mb-3">
+													<input
+														{...input}
+														type="number"
+														className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+														placeholder="Marque..."
+													/>
+												</div>
+											)}
+										</Field>
+									</div>
+
+								</div>
+								<div className="w-full"></div>
+								<div className="flex flex-wrap mt-4">
+									<div className="w-full px-3 flex-1">
 										<Field name="search">
 											{({ input, meta }) => (
 												<div className="relative w-full mb-3">
@@ -41,63 +62,125 @@ export default function AnnonceSearchForm() {
 														{...input}
 														type="text"
 														className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
-														placeholder="code postal ..."
+														placeholder="code postal de ville ou région"
 													/>
 												</div>
 											)}
 										</Field>
 									</div>
-									<div className="w-full px-4 flex-1">
+									<div className="w-full px-3 flex-1">
 										<div className="relative flex w-full flex-wrap items-stretch mb-3">
 											<Field name="question-2" component="select" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-3 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-												<option selected="selected" value="0">KM max</option>
-												<option value="25000">25 000 Km maxi</option>
-												<option value="50000">50 000 Km maxi</option>
-												<option value="75000">75 000 Km maxi</option>
-												<option value="100000">100 000 Km maxi</option>
-												<option value="125000">125 000 Km maxi</option>
-												<option value="140000">140 000 Km maxi</option>
+												{kmFilterOptions.map(kmFilterOption => (
+													<option
+														value={kmFilterOption.value}>{kmFilterOption.label}</option>
+												))}
 											</Field>
 											<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white bg-orange-500 shadow">
 												<i className="fas fa-angle-down text-2xl my-2"></i>
 											</div>
 										</div>
 									</div>
-									<div className="w-full px-4 flex-1">
+									<div className="w-full px-3 flex-1">
 										<div className="relative flex w-full flex-wrap items-stretch mb-3">
-											<Field name="question-2" component="select" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-3 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-												<option value="0" selected="selected">Prix</option>
-												<option value="3000">3 000 € maxi</option>
-												<option value="4500">4 500 € maxi</option>
-												<option value="6000">6 000 € maxi</option>
-												<option value="7500">7 500 € maxi</option>
-												<option value="9000">9 000 € maxi</option>
-												<option value="10500">10 500 € maxi</option>
-												<option value="12000">12 000 € maxi</option>
-												<option value="13500">13 500 € maxi</option>
-												<option value="15000">15 000 € maxi</option>
-												<option value="16500">16 500 € maxi</option>
-												<option value="18000">18 000 € maxi</option>
-												<option value="19500">19 500 € maxi</option>
-												<option value="21000">21 000 € maxi</option>
-												<option value="22500">22 500 € maxi</option>
-												<option value="24000">24 000 € maxi</option>
-												<option value="25500">25 500 € maxi</option>
-												<option value="27000">27 000 € maxi</option>
-												<option value="28500">28 500 € maxi</option>
-												<option value="30000">30 000 € maxi</option>
-												<option value="35000">Plus </option>
+											<Field name="km" component="select" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-3 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+												{priceFilterOptions.map(priceFilterOption => (
+													<option
+														value={priceFilterOption.value}>{priceFilterOption.label}</option>
+												))}
 											</Field>
-											<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white bg-orange-500 shadow">
+											<div
+												className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white bg-orange-500">
 												<i className="fas fa-angle-down text-2xl my-2"></i>
 											</div>
 										</div>
 									</div>
 								</div>
+								<div className="text-center mt-2">
+									<button type="button" className="mt-2 text-lg" onClick={() => setNavbarOpen(!navbarOpen)}> plus de critères ? <i
+										className="fas fa-chevron-circle-down"></i></button>
+									<div
+										className={
+											"flex flex-wrap mt-4" +
+											(navbarOpen ? " block" : " hidden")
+										}
+										id="example-navbar-warning"
+									>
+											<div className="w-full px-2 flex-1">
+												<div className="relative flex w-full flex-wrap items-stretch mb-3">
+													<Field name="energy" component="select" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-3 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+														{fuelOptions.map(fuelOption => (
+															<option
+																value={fuelOption.value}>{fuelOption.label}</option>
+														))}
+													</Field>
+													<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white bg-orange-500 shadow">
+														<i className="fas fa-angle-down text-2xl my-2"></i>
+													</div>
+												</div>
+											</div>
+											<div className="w-full px-2 flex-1">
+												<div className="relative flex w-full flex-wrap items-stretch mb-3">
+													<Field name="question-2" component="select" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-3 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+														{boiteFilterOptions.map(boiteFilterOption => (
+															<option
+																value={boiteFilterOption.value}>{boiteFilterOption.label}</option>
+														))}
+													</Field>
+													<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white bg-orange-500 shadow">
+														<i className="fas fa-angle-down text-2xl my-2"></i>
+													</div>
+												</div>
+											</div>
+											<div className="w-full px-2 flex-1">
+												<div className="relative flex w-full flex-wrap items-stretch mb-3">
+													<Field name="status" component="select" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-3 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+														{statusFilterOptions.map(statusFilterOption => (
+															<option
+																value={statusFilterOption.value}>{statusFilterOption.label}</option>
+														))}
+													</Field>
+													<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white bg-orange-500 shadow">
+														<i className="fas fa-angle-down text-2xl my-2"></i>
+													</div>
+												</div>
+											</div>
+										<div className="w-full"></div>
+										<div className="w-full px-4 flex-1 mt-2">
+											<div className="relative flex w-full flex-wrap items-stretch mb-3">
+												<Field name="status" component="select" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-3 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+													{minYearFilterOptions.map(minYearFilterOption => (
+														<option
+															value={minYearFilterOption.value}>{minYearFilterOption.label}</option>
+													))}
+												</Field>
+												<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white bg-orange-500 shadow">
+													<i className="fas fa-angle-down text-2xl my-2"></i>
+												</div>
+											</div>
+										</div>
+										<div className="w-full px-4 flex-1 mt-2">
+											<div className="relative flex w-full flex-wrap items-stretch mb-3">
+												<Field name="status" component="select" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-3 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+													{maxYearFilterOptions.map(maxYearFilterOption => (
+														<option
+															value={maxYearFilterOption.value}>{maxYearFilterOption.label}</option>
+													))}
+												</Field>
+												<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white bg-orange-500 shadow">
+													<i className="fas fa-angle-down text-2xl my-2"></i>
+												</div>
+											</div>
+										</div>
+
+
+									</div>
+								</div>
+
 							</form>
 						)}
 					/>
-					<div className="w-full px-4 flex-1 text-center">
+					<div className="w-full px-4 flex-1 text-center mt-2">
 						<button
 							className="bg-gray-800 text-white active:bg-gray-700 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
 							type="button"
