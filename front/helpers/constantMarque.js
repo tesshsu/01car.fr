1a8de2c90544a1e663a8b02261c46ba385686d8d
tesshsu <tesshsu@gmail.com>
2021-01-29 16:40:27 +0100

    #143 car, model affichange for filter input part

---
+++ front/helpers/constantMarque.js
@@ -0,0 +1,267 @@
+//page AnnonceSearchForm
+export const marqueFilterOptions = [
+    {"name": 'Toutes les marques', "code": 'Toutes les marques'},
+    {"name": 'Abarth', "code": 'Abarth'},
+    {"name": 'Aleko', "code": 'Aleko'},
+    {"name": 'Alfa Romeo', "code": 'Alfa Romeo'},
+    {"name": 'Alpina', "code": 'Alpina'},
+    {"name": 'Aro', "code": 'Aro'},
+    {"name": 'Aston Martin', "code": 'Aston Martin'},
+    {"name": 'Audi', "code": 'Audi'},
+    {"name": 'Auverland', "code": 'Auverland'},
+    {"name": 'BMW', "code": 'BMW'},
+    {"name": 'Bentley', "code": 'Bentley'},
+    {"name": 'Bertone', "code": 'Bertone'},
+    {"name": 'Buggy', "code": 'Buggy'},
+    {"name": 'Buick', "code": 'Buick'},
+    {"name": 'Cadillac', "code": 'Cadillac'},
+    {"name": 'Caterham', "code": 'Caterham'},
+    {"name": 'Chevrolet', "code": 'Chevrolet'},
+    {"name": 'Chrysler', "code": 'Chrysler'},
+    {"name": 'Citroën', "code": 'Citroën'},
+    {"name": 'Corvette', "code": 'Corvette'},
+    {"name": 'DS', "code": 'DS'},
+    {"name": 'Dacia', "code": 'Dacia'},
+    {"name": 'Daewoo', "code": 'Daewoo'},
+    {"name": 'Daihatsu', "code": 'Daihatsu'},
+    {"name": 'Daimler', "code": 'Daimler'},
+    {"name": 'Dangel', "code": 'Dangel'},
+    {"name": 'De la Chapelle', "code": 'De la Chapelle'},
+    {"name": 'Dodge', "code": 'Dodge'},
+    {"name": 'Donkervoort', "code": 'Donkervoort'},
+    {"name": 'Ferrari', "code": 'Ferrari'},
+    {"name": 'Fiat', "code": 'Fiat'},
+    {"name": 'Ford', "code": 'Ford'},
+    {"name": 'GMC', "code": 'GMC'},
+    {"name": 'Gac Gonow', "code": 'Gac Gonow'},
+    {"name": 'Honda', "code": 'Honda'},
+    {"name": 'Hummer', "code": 'Hummer'},
+    {"name": 'Hyundai', "code": 'Hyundai'},
+    {"name": 'Infiniti', "code": 'Infiniti'},
+    {"name": 'Isuzu', "code": 'Isuzu'},
+    {"name": 'Jaguar', "code": 'Jaguar'},
+    {"name": 'Jeep', "code": 'Jeep'},
+    {"name": 'Kia', "code": 'Kia'},
+    {"name": 'Lada', "code": 'Lada'},
+    {"name": 'Lamborghini', "code": 'Lamborghini'},
+    {"name": 'Lancia', "code": 'Lancia'},
+    {"name": 'Landwin', "code": 'Landwin'},
+    {"name": 'Land-Rover', "code": 'Land-Rover'},
+    {"name": 'Lexus', "code": 'Lexus'},
+    {"name": 'Lotus', "code": 'Lotus'},
+    {"name": 'MG', "code": 'MG'},
+    {"name": 'MPM Motos', "code": 'MPM Motos'},
+    {"name": 'Mahindra', "code": 'Mahindra'},
+    {"name": 'Maruti', "code": 'Maruti'},
+    {"name": 'Maserati', "code": 'Maserati'},
+    {"name": 'Maybach', "code": 'Maybach'},
+    {"name": 'Mazda', "code": 'Mazda'},
+    {"name": 'Mega', "code": 'Mega'},
+    {"name": 'Mercedes', "code": 'Mercedes'},
+    {"name": 'Mini', "code": 'Mini'},
+    {"name": 'Mitsubishi', "code": 'Mitsubishi'},
+    {"name": 'Morgan', "code": 'Morgan'},
+    {"name": 'Nissan', "code": 'Nissan'},
+    {"name": 'Opel', "code": 'Opel'},
+    {"name": 'PGO', "code": 'PGO'},
+    {"name": 'Polski/FSO', "code": 'Polski/FSO'},
+    {"name": 'Pontiac', "code": 'Pontiac'},
+    {"name": 'Porsche', "code": 'Porsche'},
+    {"name": 'Proton', "code": 'Proton'},
+    {"name": 'Renault', "code": 'Renault'},
+    {"name": 'Rolls-Royce', "code": 'Rolls-Royce'},
+    {"name": 'Rover', "code": 'Rovert'},
+    {"name": 'Saab', "code": 'Saab'},
+    {"name": 'Santana', "code": 'Santana'},
+    {"name": 'Seat', "code": 'Seat'},
+    {"name": 'Shuanghuan', "code": 'Shuanghuan'},
+    {"name": 'Skoda', "code": 'Skoda'},
+    {"name": 'Smart', "code": 'Smart'},
+    {"name": 'Ssangyong', "code": 'Ssangyong'},
+    {"name": 'Subaru', "code": 'Subaru'},
+    {"name": 'Suzuki', "code": 'Suzuki'},
+    {"name": 'TVR', "code": 'TVR'},
+    {"name": 'Talbot', "code": 'Talbot'},
+    {"name": 'Tata', "code": 'Tata'},
+    {"name": 'Tesla', "code": 'Tesla'},
+    {"name": 'Toyota', "code": 'Toyota'},
+    {"name": 'Venturi', "code": 'Venturi'},
+    {"name": 'Volkswagen', "code": 'Volkswagen'},
+    {"name": 'Volvo', "code": 'Volvo'},
+    {"name": 'Wallys', "code": 'Wallys'}
+];
+
+export const modelFilterOptions = {
+    "Toutes les marques": [
+        "*-- Tous les modèles --*"
+    ],
+    "Abarth": [
+        "500", "Abarth 124 spider", "Grande Punto"
+    ],
+    "Aleko": [
+        "2141", "Divers"
+    ],
+    "Alfa Romeo": [
+        "145", "146", "147", "155", "156", "156 Crosswagon", "156 Sportwagon", "159", "159 Sportwagon", "164", "166", "33", "4C", "75", "90", "Alfa 6", "Brera", "Giulia", "Giulietta", "GT", "GTV", "Mito", "Spider", "Stelvio", "SZ/RZ", "Divers"
+    ],
+    "Alpina": [
+        "B10", "B12", "B3", "B6", "Divers"
+    ],
+    "Aro": [
+        "Aro 10", "Aro 24", "Cross Lander", "Pick-up", "Spartana", "Trapeurs", "Divers"
+    ],
+    "Aston Martin": [
+        "Cygnet", "DB7", "DB9", "DBS", "Lagonda", "Rapide", "V8", "Vanquish", "Vantage", "V8", "Virage", "Divers"
+    ],
+    "Audi": [
+        "100", "200", "80", "90", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "Allroad","Coupé", "Q2", "Q3", "Q5", "Q7", "Q8", "R8", "RS Q5", "RS Q7", "RS Q8", "R8", "RS Q3", "RS3", "RS4", "RS5", "RS6", "S3", "S4", "S5", "S6", "S8", "SQ5", "SQ7", "SQ8", "TT", "TT S", "V8", "Divers"
+    ],
+    "Austin": [
+        "Autres", "Mini"
+    ],
+    "Auverland": [
+        "A3", "A4", "Divers"
+    ],
+    "BMW": [
+        "i3", "i8", "M2", "M3", "M4", "M5", "M6", "Série 1", "Série 2", "Série 3", "Série 4", "Série 5", "Série 6", "Série 7", "Série 8", "X1", "X2", "X3", "X4", "X5", "X6", "X7", "Z1", "Z2", "Z3", "Z4", "Z8", "Divers"
+    ],
+    "Bentley": [
+        "Arnage", "Azure", "Brooklands", "Continental", "Continental GT", "Eight", "Mulsanne", "Turbo R", "Divers"
+    ],
+    "Bertone": [
+        "Arnage", "Azure", "Brooklands", "Continental", "Continental GT", "Eight", "Mulsanne", "Turbo R", "Divers"
+    ],
+    "Buggy": [
+        "Baboulin", "DuneBuggy", "LM1"
+    ],
+    "Buick": [
+        "Park Avenue", "Divers"
+    ],
+    "Cadillac": [
+        "Allante", "ATS", "BLS", "CTS", "Eldorado", "Escalade", "Fleetwood", "Seville", "SRX", "STS", "XLR", "Divers"
+    ],
+    "Caterham": [
+        "Super Seven"
+    ],
+    "Chevrolet": [
+        "Alero", "Aveo", "Beretta", "Blazer", "Camaro", "Captiva", "Corsica", "Corvette", "Cruze", "Epica", "Evanda", "Kalos" , "Lacetti" , "Malibu" , "Nubira", "Nubira SW", "Orlando", "Rezzo", "Spark", "Tahoe", "Trans Sport", "Trax", "Volt", "Divers"
+    ],
+    "Chrysler": [
+        "300C", "300M", "Crossfire", "Es", "Grand Voyager", "Le Baron", "Neon", "New Yorker", "PT Cruiser", "Saratoga", "Sebring", "Stratus", "Viper", "Vision", "Voyager", "Divers"
+    ],
+    "Citroën": [
+        "2 CV - Dyane", "AX", "Berlingo", "BX", "C1", "C2", "C3", "C3 Aircross", "C3 Picasso", "C3 Pluriel", "C4", "C4 Aircross", "C4 cactus", "C4 Picasso", "C4 Spacetourer", "C5", "C5 aircross", "C6", "C8", "C-Crosser", "C-Elysée", "CX", "C-zero", "DS3", "DS4", "DS5", "DS6", "DS7", "E-mehari", "Evasion", "Grand C4 Picasso", "Grand C4 Spacetourer", "GS", "LNA", "Méhari", "Nemo", "Picasso", "Saxo", "Spacetourer", "Visa", "Xantia", "XM", "Xsara", "Xsara Coupe", "ZX", "Divers"
+    ],
+    "Corvette": [
+        "C6", "Z2", "ZR1"
+    ],
+    "DS": [
+        "DS3", "DS4", "DS5", "DS7"
+    ],
+    "Dacia": [
+        "Dokker", "Duster", "Lodgy", "Logan", "Sandero", "Divers"
+    ],
+    "Daewoo": [
+        "Espero", "Evanda", "Feroza", "Kalos", "Korando", "Lacetti", "Lanos", "Leganza", "Matiz", "Musso", "Nexia", "Nubira", "Rexton", "Rezzo", "Divers"
+    ],
+    "Daihatsu": [
+        "DS3", "DS4", "DS5", "DS7"
+    ],
+    "Daimler": [
+        "Autres"
+    ],
+    "Dangel": [
+        "505", "Berlingo", "C 25", "Combi", "Partner", "Divers"
+    ],
+    "De la Chapelle": [
+        "Atalante 57s", "Type 55", "Divers"
+    ],
+    "Dodge": [
+        "Avenger", "Caliber", "Journey", "Nitro", "RAM", "Viper", "Divers"
+    ],
+    "Donkervoort": [
+        "D8", "S8"
+    ],
+    "Ferrari": [
+        "308", "328", "348", "400", "412", "456", "458", "575 Maranello", "599", "612", "BB 512", "California", "F 550 Maranello", "F355", "F360", "F430", "Ff", "Mondial", "Testarossa"
+    ],
+    "Fiat": [
+        "124 spider", "127", "500", "500 C", "500 L", "500 X", "Argenta", "Barchetta", "Bertone", "Brava", "Bravo", "Cinquecento", "Coupé", "Croma", "Croma SW", "Doblo", "Fiat 600", "Fiorino", "Freemont", "Grande Punto", "Idea", "Marea", "Multipla", "Palio", "Panda", "Punto", "QUBO", "Regata", "Ritmo", "Sedici", "Seicento", "Stilo", "Tempra", "Tipo", "Ulysse", "Uno", "Divers"
+    ],
+    "Ford": [
+        "Aerostar", "B-max", "C-max", "Cougar", "Ecosport", "Edge", "Escort", "Explorer", "Fiesta", "Focus", "Focus C-MAX", "Fusion", "Galaxy", "Granada", "Grand C-MAX", "GT", "Ka", "Kuga", "Maverick", "Mondeo", "Mustang", "Orion", "Probe", "Puma", "Ranger", "Scorpio", "Sierra", "S-MAX", "Streetka", "Tourneo VP", "Transit", "Divers"
+    ],
+    "GMC": [
+        "Acadia", "Canyon", "Enjoy", "Jimmy", "Sierra", "Sonoma", "Tracker", "Yukon"
+    ],
+    "Gac Gonow": [
+        "GA200", "GA500"
+    ],
+    "Hummer": [
+        "H1", "H2", "H3"
+    ],
+    "Hyundai": [
+        "Accent", "Atos", "Azera", "Coupé", "Elantra", "Galloper", "Genesis", "Getz" ,"i10", "i20", "i30", "i40", "Ioniq", "iX20", "iX35", "iX55", "Kona", "Lantra", "Matrix", "Nexo", "Pony", "Santa Fe", "Satellite", "Scoupe", "Sonata", "Terracan", "Trajet", "Tucson", "Veloster", "XG", "Divers"
+    ],
+    "Infiniti": [
+        "EX37", "FX", "G37", "G37 Coupé", "M37", "Q30", "Q50", "Q60", "Q70", "QX30", "Qx50"
+    ],
+    "Isuzu": [
+        "D-MAX", "Trooper"
+    ],
+    "Jaguar": [
+        "Daimler", "E-PACE", "F-PACE", "F-Type", "I-PACE", "Sovereign", "S-Type", "XE", "XF", "XJ", "XJ 12", "XJ 6", "XJ 8", "XJR", "XK", "XK8", "XKR", "X-Type", "Divers"
+    ],
+    "Jeep": [
+        "Cherokee", "Commander", "Compass", "Grand Cherokee", "Laredo", "Renegade", "Willys", "Wrangler", "Divers"
+    ],
+    "Kia": [
+        "Cherokee", "Commander", "Compass", "Grand Cherokee", "Laredo", "Renegade", "Willys", "Wrangler", "Divers"
+    ],
+    "Lada": [
+        "110", "111", "1118", "112", "1500 Break", "Kalina", "Kalinka", "Natacha", "Niva", "Priora", "Sagona", "Samara", "Divers"
+    ],
+    "Lamborghini" : [
+        "Aventador", "Diablo", "Gallardo", "Huracan", "Murcielago", "Divers"
+    ],
+    "Lancia": [
+        "Beta", "Dedra", "Delta", "Flavia", "Gamma", "Kappa", "Lybra", "Musa", "Phedra", "Prisma", "Thema", "Thesis", "VOYAGER", "Y10", "Ypsilon", "Zeta", "Divers"
+    ],
+    "Landwin" : [
+        "Autres"
+    ],
+    "Land-Rover": [
+        "Defender", "Discovery", "Freelander", "Range Rover", "Range Rover Evoque", "Range rover velar", "Range Sport", "Divers"
+    ],
+    "Lexus" : [
+        "CT", "GS", "IS", "LS", "NX", "RC", "RX", "SC", "Divers"
+    ],
+    "Lotus" : [
+        "Elan", "Elise", "Esprit", "Evora", "Exige", "Divers"
+    ],
+    "MG" : [
+        "MG.F", "MG.TF", "MG.ZR", "MG.ZS", "MG.ZT", "Divers"
+    ],
+    "MPM Motos" : [
+        "Erelis"
+    ],
+    "Mahindra" : [
+        "Bolero", "CJ 340", "CJ 540", "Goa", "Goa Pick-up", "Goa", "Divers"
+    ],
+    "Maruti" : [
+        "800", "Divers"
+    ],
+    "Maserati": [
+        "222 Coupe", "3200", "430 biturbo", "Coupe", "Ghibli", "Grandsport", "Granturismo", "Levante", "Quattroporte", "Shamal", "Spider cabriolet", "Spyder", "Divers"
+    ],
+    "Maybach": [
+        "M 57", "M 62", "Divers"
+    ],
+    "Mazda": [
+        "121", "323", "626", "929", "Bt-50 Pick-up", "Cx-3", "CX-30", "CX-5", "Cx-7", "Demio", "Mazda2", "Mazda3", "Mazda5", "Mazda6", "MPV", "MX-3", "MX-5", "MX-6", "Premacy", "RX-7", "RX-8", "Tribute", "Xedos", "Divers"
+    ],
+    "Mercedes": [
+        "126", "190", "200", "230", "250", "260", "280", "300", "500", "560", "Citan", "Classe A", "Classe B", "Classe C", "Classe CL", "Classe CLA", "Classe CLC", "Classe CLS", "Classe E", "Classe G", "Classe GL", "Classe GLA", "Classe GLC", "Classe GLE", "Classe GLK", "Classe GLS", "Classe GLS", "Classe M", "Classe R", "Classe S", "Classe V", "CLK", "EQC", "G;B", "SL", "SLC", "SLK", "SLR", "SLS","Vaneo", "Viano", "Divers"
+    ],
+
+};
