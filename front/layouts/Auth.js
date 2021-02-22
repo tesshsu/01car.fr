b576a6a3de12c75b55964beceb91e7e56376bcdc
tesshsu <tesshsu@gmail.com>
2021-02-22 09:42:39 +0100

    #154 fix issue safari

--- front/layouts/Auth.js
+++ front/layouts/Auth.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-
+import {isSafari} from 'react-device-detect';
 // components
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import FooterSmall from "components/Footers/FooterSmall.js";
@@ -11,13 +11,20 @@ export default function Auth({ children }) {
       <IndexNavbar fixed />
       <main>
         <section className="relative w-full h-full py-40 min-h-screen">
-          <div
-            className="absolute top-0 w-full h-full bg-gray-900 bg-no-repeat mt-16 bg-full"
-            style={{
-              backgroundImage:
-                "url(" + require("assets/img/register_bg_2.png") + ")",
-            }}
-          ></div>
+            {isSafari ? (
+                <div
+                    className="absolute top-0 w-full h-full bg-gray-900 bg-no-repeat mt-16 bg-full"
+                ></div>
+            ):(
+                <div
+                    className="absolute top-0 w-full h-full bg-gray-900 bg-no-repeat mt-16 bg-full"
+                    style={{
+                        backgroundImage:
+                            "url(" + require("assets/img/register_bg_2.png") + ")",
+                    }}
+                ></div>
+            )}
+
           {children}
           <FooterSmall absolute />
         </section>
